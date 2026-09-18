b67a1e6f8a7b8e258cf770fa1ce39755dccdb88d
Denis Roussel <denis.roussel@acsone.eu>
2026-09-18 11:23:05 +0200

    [IMP] connector_importer_api: Don't use new cursor for debug tests

--- connector_importer_api/components/importer.py
+++ connector_importer_api/components/importer.py
@@ -1,7 +1,7 @@
 # Copyright ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from odoo.addons.component.core import Component
+from odoo.addons.connector_importer.models.recordset import ImportRecordset
 
 
 class RecordSetImporter(Component):
@@ -9,8 +9,8 @@ class RecordSetImporter(Component):
 
     _inherit = "importer.recordset"
 
-    def create_and_run(self, chunk, recordset):
+    def create_and_run(self, chunk, recordset: ImportRecordset):
         # If stream, as soon as a chunk is ready, run the import of records
-        if recordset.source_ref_id.stream:
+        if not recordset.backend_id.debug_mode and recordset.source_ref_id.stream:
             return self._create_and_run(chunk, recordset, new_cr=True)
         return super().create_and_run(chunk, recordset)
